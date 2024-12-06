ad987db13dbbdd66442aa856b24c5b7812305cfd
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-12-06 20:01:28 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿﻿string fraseDigitada, fraseMemeToNervoso;
+Console.Clear();
+Console.Write("Usuário, o que você pensa sobre mim? ");
+fraseDigitada = Console.ReadLine()!;
+
+fraseMemeToNervoso =
+    $"{fraseDigitada.Remove(10)}... NÃO, PERA.\nTô nervoso";
+
+Console.WriteLine(fraseMemeToNervoso);
